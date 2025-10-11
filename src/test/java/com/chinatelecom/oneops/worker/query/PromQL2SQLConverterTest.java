1330df9d7b1e2da7c4bb3f8f4c39f877ce09a1d5
chaoli <lichao01.ah@chinatelecom.cn>
2025-10-11 22:30:48 +0800

    整合测试用例

--- src/test/java/com/chinatelecom/oneops/worker/query/PromQL2SQLConverterTest.java
+++ src/test/java/com/chinatelecom/oneops/worker/query/PromQL2SQLConverterTest.java
@@ -59,66 +59,6 @@ public class PromQL2SQLConverterTest {
         }
     }
 
-
-    @Test
-    public void testConvert() throws IOException{
-        String[] testSources=new String[]{
-            "aggregation5",
-            "aggregation4",
-            "aggregation3",
-            "aggregation2",
-            "aggregation1",
-            "duration",
-            "at_modifier_constant",
-            "function3",
-            "function2",
-            "function1",
-            "offset",
-            "matrix_selector3",
-            "matrix_selector2",
-            "matrix_selector1",
-            "instant_selector1",
-            "instant_selector2",
-            "instant_selector3",
-            "instant_selector4"
-        };
-        String[] testResults = new String[] {
-                "select a2.receivetime,a2.mode,(sum(a2.node_cpu_seconds_total))*(100) node_cpu_seconds_total from (select a1.receivetime,a1.cpu,a1.mode,safeDiv(a1.node_cpu_seconds_total-lag(a1.node_cpu_seconds_total) over (partition by a1.cpu,a1.mode order by a1.receivetime),extract(epoch from (a1.receivetime-lag(a1.receivetime) over (partition by a1.cpu,a1.mode order by a1.receivetime)))) node_cpu_seconds_total from (select a0.receivetime,a0.cpu,a0.mode,last(a0.node_cpu_seconds_total,a0.receivetime) node_cpu_seconds_total from linux_cpu_mode_info a0 where a0.receivetime between now() - interval '5 min' and now() and a0.object_id='6cce615e-8afa-4bde-a648-b4163a0ee957' group by a0.receivetime,a0.cpu,a0.mode order by a0.receivetime asc,a0.cpu asc,a0.mode asc) a1) a2 group by a2.receivetime,a2.mode",
-                "select a5.receivetime,a5.mode,(a5.node_cpu_seconds_total)/(a6.node_cpu_seconds_total) node_cpu_seconds_total from (select a2.receivetime,a2.mode,(sum(a2.node_cpu_seconds_total))*(100) node_cpu_seconds_total from (select a1.receivetime,a1.cpu,a1.mode,safeDiv(a1.node_cpu_seconds_total-lag(a1.node_cpu_seconds_total) over (partition by a1.cpu,a1.mode order by a1.receivetime),extract(epoch from (a1.receivetime-lag(a1.receivetime) over (partition by a1.cpu,a1.mode order by a1.receivetime)))) node_cpu_seconds_total from (select a0.receivetime,a0.cpu,a0.mode,last(a0.node_cpu_seconds_total,a0.receivetime) node_cpu_seconds_total from linux_cpu_mode_info a0 where a0.receivetime between now() - interval '5 min' and now() and a0.object_id='6cce615e-8afa-4bde-a648-b4163a0ee957' group by a0.receivetime,a0.cpu,a0.mode order by a0.receivetime asc,a0.cpu asc,a0.mode asc) a1) a2 group by a2.receivetime,a2.mode) a5 INNER JOIN (select a4.receivetime,a4.mode,count(a4.node_cpu_seconds_total) node_cpu_seconds_total from (select a3.receivetime,a3.cpu,a3.mode,last(a3.node_cpu_seconds_total,a3.receivetime) node_cpu_seconds_total from linux_cpu_mode_info a3 where a3.receivetime=(select receivetime from linux_cpu_mode_info where receivetime>now() - interval '2 min' and receivetime<now() and object_id='6cce615e-8afa-4bde-a648-b4163a0ee957' order by receivetime desc limit 1) and a3.object_id='6cce615e-8afa-4bde-a648-b4163a0ee957' group by a3.receivetime,a3.cpu,a3.mode order by a3.receivetime asc,a3.cpu asc,a3.mode asc) a4 group by a4.receivetime,a4.mode) a6 on (a5.receivetime=a6.receivetime and a5.mode=a6.mode) ",
-                "select a1.receivetime,a1.cpu,a1.mode,last(a1.node_cpu_seconds_total,a1.receivetime) node_cpu_seconds_total from (select a0.receivetime,a0.cpu,a0.mode,last(a0.node_cpu_seconds_total,a0.receivetime) node_cpu_seconds_total from linux_cpu_mode_info a0 where a0.receivetime between now() - interval '1 min' and now() group by a0.receivetime,a0.cpu,a0.mode order by a0.receivetime asc,a0.cpu asc,a0.mode asc) a1 group by a1.receivetime,a1.cpu,a1.mode",
-                "select a1.receivetime,a1.node_cpu_seconds_total total_count,a1.mode,count(1) node_cpu_seconds_total from (select a0.receivetime,a0.cpu,a0.mode,last(a0.node_cpu_seconds_total,a0.receivetime) node_cpu_seconds_total from linux_cpu_mode_info a0 where a0.receivetime=(select receivetime from linux_cpu_mode_info where receivetime>now() - interval '2 min' and receivetime<now() order by receivetime desc limit 1) group by a0.receivetime,a0.cpu,a0.mode order by a0.receivetime asc,a0.cpu asc,a0.mode asc) a1 group by a1.receivetime,a1.node_cpu_seconds_total,a1.mode",
-                "select a1.receivetime,a1.mode,count(a1.node_cpu_seconds_total) node_cpu_seconds_total from (select a0.receivetime,a0.cpu,a0.mode,last(a0.node_cpu_seconds_total,a0.receivetime) node_cpu_seconds_total from linux_cpu_mode_info a0 where a0.receivetime=(select receivetime from linux_cpu_mode_info where receivetime>now() - interval '2 min' and receivetime<now() and object_id='6cce615e-8afa-4bde-a648-b4163a0ee957' order by receivetime desc limit 1) and a0.object_id='6cce615e-8afa-4bde-a648-b4163a0ee957' group by a0.receivetime,a0.cpu,a0.mode order by a0.receivetime asc,a0.cpu asc,a0.mode asc) a1 group by a1.receivetime,a1.mode",
-                "select a0.receivetime,a0.namespace,a0.pod,last(a0.container_cpu_usage_seconds_total,a0.receivetime) container_cpu_usage_seconds_total from container_pod_cpu_info a0 where a0.receivetime=(select receivetime from container_pod_cpu_info where receivetime>now() - interval '1 year 5 min 900 millisecond' - interval '2 min' and receivetime<now() - interval '1 year 5 min 900 millisecond' order by receivetime desc limit 1) group by a0.receivetime,a0.namespace,a0.pod order by a0.receivetime asc,a0.namespace asc,a0.pod asc",
-                "select a0.receivetime,a0.cpu,a0.mode,last(a0.node_cpu_seconds_total,a0.receivetime) node_cpu_seconds_total from linux_cpu_mode_info a0 where a0.receivetime=(select receivetime from linux_cpu_mode_info where receivetime>TO_TIMESTAMP(1609746000) - interval '2 min' and receivetime<TO_TIMESTAMP(1609746000) order by receivetime desc limit 1) group by a0.receivetime,a0.cpu,a0.mode order by a0.receivetime asc,a0.cpu asc,a0.mode asc",
-                "select a1.receivetime,clamp(a1.node_load1,0,10) node_load1 from (select a0.receivetime,last(a0.node_load1,a0.receivetime) node_load1 from linux_info a0 where a0.receivetime=(select receivetime from linux_info where receivetime>now() - interval '2 min' and receivetime<now() order by receivetime desc limit 1) group by a0.receivetime order by a0.receivetime asc) a1",
-                "select a2.receivetime,a2.cpu,a2.mode,sin(a2.node_cpu_seconds_total) node_cpu_seconds_total from (select a1.receivetime,a1.cpu,a1.mode,safeDiv(a1.node_cpu_seconds_total-lag(a1.node_cpu_seconds_total) over (partition by a1.cpu,a1.mode order by a1.receivetime),extract(epoch from (a1.receivetime-lag(a1.receivetime) over (partition by a1.cpu,a1.mode order by a1.receivetime)))) node_cpu_seconds_total from (select a0.receivetime,a0.cpu,a0.mode,last(a0.node_cpu_seconds_total,a0.receivetime) node_cpu_seconds_total from linux_cpu_mode_info a0 where a0.receivetime between now() - interval '5 min' and now() group by a0.receivetime,a0.cpu,a0.mode order by a0.receivetime asc,a0.cpu asc,a0.mode asc) a1) a2",
-                "select a2.device,extract (day from a2.receivetime) receivetime from (select a1.device,a1.receivetime from (select a0.receivetime,a0.device,last(a0.node_network_up,a0.receivetime) node_network_up from linux_device_info a0 where a0.receivetime=(select receivetime from linux_device_info where receivetime>now() - interval '2 min' and receivetime<now() and device='eth0' order by receivetime desc limit 1) and a0.device='eth0' group by a0.receivetime,a0.device order by a0.receivetime asc,a0.device asc) a1) a2",
-                "select a0.receivetime,a0.namespace,a0.pod,last(a0.container_cpu_usage_seconds_total,a0.receivetime) container_cpu_usage_seconds_total from container_pod_cpu_info a0 where a0.receivetime=(select receivetime from container_pod_cpu_info where receivetime>now() - interval '5 min' - interval '2 min' and receivetime<now() - interval '5 min' order by receivetime desc limit 1) group by a0.receivetime,a0.namespace,a0.pod order by a0.receivetime asc,a0.namespace asc,a0.pod asc",
-                "select time_bucket('1 min',a0.receivetime) receivetime,a0.namespace,a0.pod,last(a0.container_cpu_usage_seconds_total,a0.receivetime) container_cpu_usage_seconds_total from container_pod_cpu_info a0 where a0.receivetime between now() - interval '5 min' and now() group by time_bucket('1 min',a0.receivetime),a0.namespace,a0.pod order by time_bucket('1 min',a0.receivetime) asc,a0.namespace asc,a0.pod asc",
-                "select a0.receivetime,a0.namespace,a0.pod,last(a0.container_cpu_usage_seconds_total,a0.receivetime) container_cpu_usage_seconds_total from container_pod_cpu_info a0 where a0.receivetime between now() - interval '5 min' and now() group by a0.receivetime,a0.namespace,a0.pod order by a0.receivetime asc,a0.namespace asc,a0.pod asc",
-                "select a0.receivetime,a0.namespace,a0.pod,last(a0.container_cpu_usage_seconds_total,a0.receivetime) container_cpu_usage_seconds_total from container_pod_cpu_info a0 where a0.receivetime between now() - interval '5 min' and now() group by a0.receivetime,a0.namespace,a0.pod order by a0.receivetime asc,a0.namespace asc,a0.pod asc",
-                "select a0.receivetime,a0.namespace,a0.pod,last(a0.container_cpu_usage_seconds_total,a0.receivetime) container_cpu_usage_seconds_total from container_pod_cpu_info a0 where a0.receivetime=(select receivetime from container_pod_cpu_info where receivetime>now() - interval '2 min' and receivetime<now() order by receivetime desc limit 1) group by a0.receivetime,a0.namespace,a0.pod order by a0.receivetime asc,a0.namespace asc,a0.pod asc",
-                "select a0.receivetime,a0.namespace,a0.pod,last(a0.container_cpu_usage_seconds_total,a0.receivetime) container_cpu_usage_seconds_total from container_pod_cpu_info a0 where a0.receivetime=(select receivetime from container_pod_cpu_info where receivetime>now() - interval '2 min' and receivetime<now() order by receivetime desc limit 1) group by a0.receivetime,a0.namespace,a0.pod order by a0.receivetime asc,a0.namespace asc,a0.pod asc",
-                "select a0.receivetime,a0.namespace,a0.pod,last(a0.container_cpu_usage_seconds_total,a0.receivetime) container_cpu_usage_seconds_total from container_pod_cpu_info a0 where a0.receivetime=(select receivetime from container_pod_cpu_info where receivetime>now() - interval '2 min' and receivetime<now() and job='kubelet' order by receivetime desc limit 1) and a0.job='kubelet' group by a0.receivetime,a0.namespace,a0.pod order by a0.receivetime asc,a0.namespace asc,a0.pod asc",
-                "select a0.receivetime,a0.namespace,a0.pod,last(a0.container_cpu_usage_seconds_total,a0.receivetime) container_cpu_usage_seconds_total from container_pod_cpu_info a0 where a0.receivetime=(select receivetime from container_pod_cpu_info where receivetime>now() - interval '2 min' and receivetime<now() order by receivetime desc limit 1) group by a0.receivetime,a0.namespace,a0.pod order by a0.receivetime asc,a0.namespace asc,a0.pod asc",
-        };
-        IMetricFinder finder=new IMetricFinder(){
-            public String findTableName(String metricName) {
-                return metricMap.get(metricName);
-            }
-            public List<String> getMetricLabels(String tableName){
-                return metricLabelMap.get(tableName);
-            } 
-        };
-        for (int i=0;i<testSources.length;i++) {
-            PromQL2SQLConverter converter = new PromQL2SQLConverter(finder);
-            String sourceText=loadTestContent(testSources[i] + ".txt");
-            String result=converter.convertPromQL(sourceText,"now()","now()");
-            assertTrue(String.format("转化文件%s中PromQL语句%s,\n结果是到%s\n期望是%s",testSources[i],sourceText,result,testResults[i]),
-                result.equals(testResults[i]));   
-        }
-    }
-
     private String loadTestContent(String filePath) throws IOException{
         ClassLoader classLoader = PromQL2SQLConverterTest.class.getClassLoader();
         InputStream inputStream = classLoader.getResourceAsStream(filePath);
